9a79bba9742a3adea02759523506d851073972f4
JoannaKozlowska <joanna.kozlowska2016@gmail.com>
2019-01-15 21:49:49 +0100

    Excersise 1

---
+++ src/main/java/partone/Letter.java
@@ -0,0 +1,11 @@
+package partone;
+
+public class Letter {
+    public static void main(String[] args) {
+        String str1 = "Try...";
+        char letters[] = {'a', 'n', 'd'};
+        String str2 = new String(letters);
+        String str3 = new String("see what happens.");
+        System.out.println(str1 + "\n"+ str2 + " " + str3);
+    }
+}
